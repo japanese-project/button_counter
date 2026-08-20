6ef61a3d6c49517037e1c079588fb9cae3839ae5
SREANGHOUR HY <sreanghour.hygeneral007@gmail.com>
2026-08-20 14:59:49 +0700

    using tailwind CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Header from '../components/Header.svelte';
+	import NumberDisplay from '../components/Number.svelte';
+	import CounterButton from '../components/Button.svelte';
+    
+
+	let count: number = $state(0);
+
+	function incrementCounter(): void {
+		count += 1;
+	}
+
+</script>
+
+<svelte:head>
+	<title>Button Counter</title>
+	<meta
+		name="description"
+		content="A button counter built with SvelteKit, TypeScript, and Tailwind CSS"
+	/>
+</svelte:head>
+
+<div class="min-h-screen bg-stone-100 text-gray-900">
+	<Header />
+
+	<main
+		class="flex min-h-[calc(100vh-57px)] flex-col items-center justify-center gap-8 px-4 py-8"
+	>
+		<NumberDisplay {count} />
+
+		<CounterButton onTrigger={incrementCounter} />
+	</main>
+</div>
