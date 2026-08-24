57f9df01e78462778daefc64307e0bf7e68c1bb2
socheatcheng <cheng.socheat@elitevigour.com>
2026-08-24 18:48:22 +0700

    add prettier

--- prettier.config.js
+++ prettier.config.js
@@ -2,7 +2,8 @@
 const config = {
 	useTabs: true,
 	singleQuote: true,
-	trailingComma: 'none',
+	semi:false,
+	trailingComma: 'all',
 	printWidth: 100,
 	plugins: ['prettier-plugin-svelte'],
 	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
